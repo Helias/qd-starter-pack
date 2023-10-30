541b0f0e6504cebee226e12c89961fdf4d16caf1
Giuseppe Scordo <peppescordo03@gmail.com>
2023-10-30 17:39:45 +0100

    feat: calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Insert first number: ")
+x = int(input())
+print("Insert second number: ")
+y = int(input())
+
+print("Sum: " + str(x + y))
+print("Difference: " + str(x - y))
+print("Multiplication: " + str(x * y))
+print("Division: " + str(x / y))
