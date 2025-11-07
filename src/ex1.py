3377a70b8d94045e919d2f106d6b4d9cd55b630f
Helias <stefanoborzi32@gmail.com>
2025-11-07 21:58:54 +0100

    feat(exercises): load python exercises

---
+++ src/ex1.py
@@ -0,0 +1,15 @@
+# Implement this exercise from C++ to Python
+
+# #include <iostream>
+# using namespace std;
+#
+# int main()
+# {
+#   int week;
+#
+#   /* Input week number from user */
+#   cout << "Enter week number(1-7): " << endl;
+#   cin >> week;
+#
+#   return 0;
+# }
